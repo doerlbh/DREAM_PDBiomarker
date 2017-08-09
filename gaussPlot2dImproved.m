2d759b9c8f3f64f26ff0492462d2984a7c43ef05
doerlbh <doerlbh@gmail.com>
2017-08-09 12:20:29 -0400

    update code

--- gaussPlot2dImproved.m
+++ gaussPlot2dImproved.m
@@ -1,9 +1,9 @@
-function h=gaussPlot3d(mu, Sigma, varargin)
+function h=gaussPlot2dImproved(mu, Sigma, varargin)
 % Plot an ellipse representing the covariance matrix of a Gaussian
 
 % This file is from pmtk3.googlecode.com
 
-if size(Sigma) ~= [3 3], error('Sigma must be a 3 by 3 matrix'); end
+if size(Sigma) ~= [2 2], error('Sigma must be a 2 by 2 matrix'); end
 [color, plotMarker] = process_options(varargin, 'color', 'b', 'plotMarker', true);
 
 mu = mu(:);
@@ -15,10 +15,10 @@ xy = [cos(t); sin(t)];
 k = sqrt(6);
 w = (k * U * sqrt(D)) * xy;
 z = repmat(mu, [1 n]) + w;
-h = plot3(z(1, :), z(2, :),z(3, :),'color', color, 'linewidth', 2);
+h = plot(z(1, :), z(2, :), 'color', color, 'linewidth', 2);
 hold on
 if plotMarker
-  hh=plot3(mu(1), mu(2), mu(3),  'x');
+  hh=plot(mu(1), mu(2),  'x');
   set(hh,'color',color, 'linewidth', 2,  'markersize', 13);
 end
 %axis('equal');    
